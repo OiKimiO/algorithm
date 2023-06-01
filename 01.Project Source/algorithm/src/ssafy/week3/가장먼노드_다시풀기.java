08da07c228bcbe1ff3e5a58ff01d9523862eb3b5
OiKimiO <53805469+OiKimiO@users.noreply.github.com>
2023-06-01 18:03:11 +0900

    Add #6 - Week 3 진행중

---
+++ 01.Project Source/algorithm/src/ssafy/week3/가장먼노드_다시풀기.java
@@ -0,0 +1,52 @@
+package ssafy.week3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 가장먼노드_다시풀기 {
+    public static void main(String[] args) {
+        int n = 6;
+        int [][] arr = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+
+        solution(n, arr);
+    }
+
+    private static void solution(int n, int[][] arr) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        boolean[] arr_bool = new boolean[n+1];
+        int[] distance     = new int[n+1];
+
+        for (int i = 0; i <= arr.length; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            list.get(arr[i][0]).add(arr[i][1]);
+            list.get(arr[i][1]).add(arr[i][0]);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+
+
+        while(!queue.isEmpty()){
+            int number = queue.poll();
+            ArrayList<Integer> node = list.get(number);
+            if(!arr_bool[number]){
+                for (int i = 0; i < node.size(); i++) {
+                    arr_bool[number] = true;
+                    queue.add(node.get(i));
+                    distance[number] = distance[number] + 1;
+                }
+            }
+        }
+
+        Arrays.sort(distance);
+
+        for (int i = 0; i < distance.length; i++) {
+            System.out.println(distance[i]);
+        }
+    }
+}
